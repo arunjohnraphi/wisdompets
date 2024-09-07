69f0f865027fe43ab049d9e54039055b2042781a
arunjohnraphi <arunjohnraphi@gmail.com>
2024-09-07 21:03:36 +0530

    Pet model added in models.py

--- adoptions/models.py
+++ adoptions/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Pet(models.model):
+    SEX_CHOICES = [('M','Male'),('F','Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30, blank= True)
+    descriptions = models.TextField()
+    sex = models.CharField(max_length=1,choice=SEX_CHOICES,blank=True)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True)
+
+
+
